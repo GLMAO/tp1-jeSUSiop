ba4dc1dbb7bcef746db3dbd7c1940a9c7f31d7ef
rahmanou <abderrahmane.bel12@gmail.com>
2025-11-02 15:33:05 +0100

    dernier

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -31,16 +31,16 @@ public class App {
     private static void testDuTimeService(TimerService timerService) {
        // Horloge horloge = new Horloge("Num 1",timerService) ;
        // Horloge horloge2 = new Horloge("Num 2",timerService) ;
-        CompteARebours compteARebours = new CompteARebours(timerService,10,"Compte A Rebours 1") ;
-        CompteARebours compteARebours1 = new CompteARebours(timerService,15,"Compte A Rebours 1") ;
-        CompteARebours compteARebours2 = new CompteARebours(timerService,20,"Compte A Rebours 1") ;
-        CompteARebours compteARebours3 = new CompteARebours(timerService,12,"Compte A Rebours 1") ;
-        CompteARebours compteARebours4 = new CompteARebours(timerService,13,"Compte A Rebours 1") ;
-        CompteARebours compteARebours5 = new CompteARebours(timerService,14,"Compte A Rebours 1") ;
-        CompteARebours compteARebours6 = new CompteARebours(timerService,17,"Compte A Rebours 1") ;
-        CompteARebours compteARebours7 = new CompteARebours(timerService,18,"Compte A Rebours 1") ;
-        CompteARebours compteARebours8 = new CompteARebours(timerService,11,"Compte A Rebours 1") ;
-        CompteARebours compteARebours9 = new CompteARebours(timerService,16,"Compte A Rebours 1") ;
+        CompteARebours compteARebours = new CompteARebours(timerService,10,"Compte A Rebours ") ;
+        CompteARebours compteARebours1 = new CompteARebours(timerService,15,"Compte A Rebours1 ") ;
+        CompteARebours compteARebours2 = new CompteARebours(timerService,20,"Compte A Rebours 2") ;
+        CompteARebours compteARebours3 = new CompteARebours(timerService,12,"Compte A Rebours 3") ;
+        CompteARebours compteARebours4 = new CompteARebours(timerService,13,"Compte A Rebours 4") ;
+        CompteARebours compteARebours5 = new CompteARebours(timerService,14,"Compte A Rebours 5") ;
+        CompteARebours compteARebours6 = new CompteARebours(timerService,17,"Compte A Rebours 6") ;
+        CompteARebours compteARebours7 = new CompteARebours(timerService,18,"Compte A Rebours 7") ;
+        CompteARebours compteARebours8 = new CompteARebours(timerService,11,"Compte A Rebours 8") ;
+        CompteARebours compteARebours9 = new CompteARebours(timerService,16,"Compte A Rebours 9") ;
     }
 
     public static void clearScreen() {
